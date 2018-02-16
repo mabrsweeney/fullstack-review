750714bf8f08f81363fed05a5e35f2f1792eb354
mabrsweeney <mabrsweeney@gmail.com>
2018-02-16 15:48:13 -0800

    bound component methods

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,6 +8,7 @@ class Search extends React.Component {
     }
   }
 
+  //As the user types store the text from the input form
   onChange (e) {
     this.setState({
       term: e.target.value
@@ -21,8 +22,8 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)}/>       
+      <button onClick={this.search.bind(this)}> Add Repos </button>
     </div>) 
   }
 }
