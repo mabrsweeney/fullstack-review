47b1f14ffd8fee356de76854b0971a2e2cfc56e9
mabrsweeney <mabrsweeney@gmail.com>
2018-02-16 15:48:24 -0800

    renders repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    { props.repos.map((repo, index) => {
+      return <div key={index} className="repo">
+        <h2><a href={repo.url}>{repo.reponame}</a></h2>
+        <p>{'ID: '+repo.id}</p>
+        <p>{'User: '+repo.username}</p>
+        <p>{'Description: '+repo.description}</p>
+      </div>
+    })}
   </div>
 )
 
