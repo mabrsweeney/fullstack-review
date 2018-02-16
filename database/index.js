ced7723f263004284aa97b00ff79442338f385e5
mabrsweeney <mabrsweeney@gmail.com>
2018-02-16 15:48:43 -0800

    now gets and saves repos

--- database/index.js
+++ database/index.js
@@ -2,15 +2,24 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number, reponame: String, username: String, description: String, url: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  for(var i in repos){
+    var mod = new Repo({id:repos[i].id, reponame:repos[i].name, 
+      username:repos[i].owner.login, description:repos[i].description, url:repos[i].html_url });
+    mod.save();
+  }
+}
+let getRepos = function(callback){
+  Repo.find().sort({reponame:1}).limit(25).exec(function(err, repos){
+    if (err) throw err;
+    callback({repos:repos});
+  })
 }
 
 module.exports.save = save;
+module.exports.getRepos = getRepos;
